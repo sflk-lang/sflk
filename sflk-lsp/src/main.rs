b3ae612ae31a96125d8628823e1c25969d876371
Arthur Correnson <arthur.correnson@gmail.com>
2021-02-07 17:59:08 +0100

    Update rustfmt file

--- sflk-lsp/src/main.rs
+++ sflk-lsp/src/main.rs
@@ -6,57 +6,57 @@ mod completion;
 
 #[derive(Debug)]
 struct Backend {
-    client: Client,
+	client: Client,
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult {
-            capabilities: ServerCapabilities {
-                completion_provider: Some(CompletionOptions {
-                    trigger_characters: Some(vec![
-                        " ".to_string(),
-                        "=".to_string(),
-                        "\n".to_string(),
-                    ]),
-                    ..Default::default()
-                }),
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-    }
+	async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+		Ok(InitializeResult {
+			capabilities: ServerCapabilities {
+				completion_provider: Some(CompletionOptions {
+					trigger_characters: Some(vec![
+						" ".to_string(),
+						"=".to_string(),
+						"\n".to_string(),
+					]),
+					..Default::default()
+				}),
+				..Default::default()
+			},
+			..Default::default()
+		})
+	}
 
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::Info, "sflk server initialized (uwu) !")
-            .await;
-    }
+	async fn initialized(&self, _: InitializedParams) {
+		self.client
+			.log_message(MessageType::Info, "sflk server initialized (uwu) !")
+			.await;
+	}
 
-    async fn shutdown(&self) -> Result<()> {
-        Ok(())
-    }
+	async fn shutdown(&self) -> Result<()> {
+		Ok(())
+	}
 
-    async fn completion(&self, _params: CompletionParams) -> Result<Option<CompletionResponse>> {
-        Ok(Some(CompletionResponse::Array(vec![
-            completion::symb("pr"),
-            completion::symb("nl"),
-            completion::symb("do"),
-            completion::symb("if"),
-            completion::symb("redo"),
-        ])))
-    }
+	async fn completion(&self, _params: CompletionParams) -> Result<Option<CompletionResponse>> {
+		Ok(Some(CompletionResponse::Array(vec![
+			completion::symb("pr"),
+			completion::symb("nl"),
+			completion::symb("do"),
+			completion::symb("if"),
+			completion::symb("redo"),
+		])))
+	}
 }
 
 #[tokio::main]
 async fn main() {
-    let stdin = tokio::io::stdin();
-    let stdout = tokio::io::stdout();
+	let stdin = tokio::io::stdin();
+	let stdout = tokio::io::stdout();
 
-    let (service, messages) = LspService::new(|client| Backend { client });
-    Server::new(stdin, stdout)
-        .interleave(messages)
-        .serve(service)
-        .await;
+	let (service, messages) = LspService::new(|client| Backend { client });
+	Server::new(stdin, stdout)
+		.interleave(messages)
+		.serve(service)
+		.await;
 }
