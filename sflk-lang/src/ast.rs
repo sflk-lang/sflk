e4bf6db4914d272605379b6f016d12b4aa7ab0e8
anima-libera <anim.libera@gmail.com>
2021-02-09 20:45:36 +0100

    Prepare the AST architecture

--- sflk-lang/src/ast.rs
+++ sflk-lang/src/ast.rs
@@ -1,1 +1,11 @@
-// ah shit, here we go again
+use crate::scu::Loc;
+
+// TODO:
+// - move Loc here
+// - delete Located
+// - parser->program must become parser->ast->program
+
+struct MetaNode<T> {
+	node: T,
+	loc: Loc,
+}
--- sflk-lang/src/main.rs
+++ sflk-lang/src/main.rs
@@ -1,3 +1,4 @@
+mod ast;
 mod log;
 mod machine;
 mod object;
