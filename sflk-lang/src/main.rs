78c8745d1fe35208e35de2c68a8e1a19a60d5be5
anima-libera <anim.libera@gmail.com>
2022-11-21 23:49:51 +0100

    Commented the settings module

--- sflk-lang/src/main.rs
+++ sflk-lang/src/main.rs
@@ -23,23 +23,25 @@ fn main() {
 	// Parse the command line arguments.
 	let settings = Settings::from_args();
 	settings.print_info();
-	if settings.src_code().is_none() {
+	if settings.src.is_none() {
 		return;
 	}
 
 	// Get the source code in memory.
-	let scu = Rc::new(match settings.src_code() {
-		Some(Source::FilePath(file_path)) => SourceCodeUnit::from_filename(file_path),
-		Some(Source::Code(code)) => SourceCodeUnit::from_str(code.to_string(), "input".to_string()),
+	let scu = Rc::new(match settings.src {
+		Some(Source::FilePath(ref file_path)) => SourceCodeUnit::from_filename(file_path),
+		Some(Source::Code(ref code)) => {
+			SourceCodeUnit::from_str(code.to_string(), "input".to_string())
+		},
 		None => panic!(),
 	});
 
 	// Get the tokenizer ready.
 	let tfr = TokBuffer::from(CharReadingHead::from_scu(scu));
 
-	if settings.display_tokens() {
+	if settings.display_tokens {
 		// Don't execute any code, only display tokens.
-		tfr.display_all(settings.debug_lines());
+		tfr.display_all(settings.debug_lines);
 		return;
 	}
 
@@ -49,13 +51,13 @@ fn main() {
 
 	// Parse the source code into an AST.
 	let ast = parser.parse_program();
-	if settings.debug() {
+	if settings.debug {
 		ast.print();
 	}
 
 	// Transform the AST into SIR code.
 	let sir_block = sir::program_to_sir_block(ast.unwrap_ref());
-	if settings.debug_sir() {
+	if settings.debug_sir {
 		dbg!(&sir_block);
 	}
 
